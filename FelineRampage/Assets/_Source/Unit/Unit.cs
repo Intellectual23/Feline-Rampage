d22c6c894c21f27c0981608ea87e9454e72aaedd
AlinaMalinafff <150148650+AlinaMalinafff@users.noreply.github.com>
2024-03-19 13:02:32 +0300

    feat(code): FightMode in Unit created

--- FelineRampage/Assets/_Source/Unit/Unit.cs
+++ FelineRampage/Assets/_Source/Unit/Unit.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.Design.Serialization;
+﻿using System;
+using System.ComponentModel.Design.Serialization;
 
 namespace Unit
 {
@@ -9,6 +10,82 @@ namespace Unit
     public Unit(UnitSettings unitSettings)
     {
       _unitSettings = unitSettings;
+    }
+
+    public void FightMode()
+    {
+      UnitSettings firstAttacker;
+      UnitSettings secondAttacker;
+      
+      Random rnd = new Random();
+      int whoIsFirst = rnd.Next(0, 1);
+      if (whoIsFirst == 0)
+      {
+        firstAttacker = Game.Instance.Settings;
+        secondAttacker = _unitSettings;
+      }
+      else
+      {
+        firstAttacker = _unitSettings;
+        secondAttacker = Game.Instance.Settings;
+      }
+      
+      while (true)
+      {
+        int firstDamage = CalculateDamage(firstAttacker, rnd);
+        // dodged? or receive damage and is dead?
+        if (!ReceiveDamage(secondAttacker, rnd, firstDamage))
+        {
+          // if dead
+          break;
+        }
+        int secondDamage = CalculateDamage(secondAttacker, rnd);
+        // dodged? or receive? is dead?
+        if (!ReceiveDamage(firstAttacker, rnd, secondDamage))
+        {
+          break;
+        }
+      }
+    }
+
+    private bool ReceiveDamage(UnitSettings unit, Random rnd, int damage)
+    {
+      int dodgeProbability = (unit.Agility / 12) * 100;
+      int cubeParameter = rnd.Next(1, 100);
+      
+      // successfully dodged
+      if (cubeParameter >= 1 && cubeParameter < dodgeProbability)
+      {
+        return true;
+      }
+
+      unit.Hp -= damage;
+      if (unit.Hp <= 0)
+      {
+        return false;
+      }
+      return true;
+    }
+    
+    private int CalculateDamage(UnitSettings unit, Random rnd)
+    {
+      int defaultDamage = unit.Strength * 5;
+      int cubeParam = rnd.Next(1, 100);
+      int defaultAttackPercentage = 50;
+      int criticalAttackPercentage = 10 * unit.Luck;
+      int missedAttackPercentage = 100 - defaultAttackPercentage - criticalAttackPercentage;
+      // промах
+      if (cubeParam >= 1 && cubeParam < missedAttackPercentage)
+      {
+        return 0;
+      }
+      // дефолт атака
+      if (cubeParam >= missedAttackPercentage && cubeParam < missedAttackPercentage + defaultAttackPercentage)
+      {
+        return defaultDamage;
+      }
+
+      return 2 * defaultDamage;
     }
   }
 }
