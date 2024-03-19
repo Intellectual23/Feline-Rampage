dac055f0c7fa4a9e8efcf39c3117bf15cf844257
Egor <68235201+Intellectual23@users.noreply.github.com>
2024-03-19 10:55:10 +0300

    feat(code): create Game class
    1) Create Singletone class Game

--- FelineRampage/Assets/_Source/Unit/Unit.cs
+++ FelineRampage/Assets/_Source/Unit/Unit.cs
@@ -1,9 +1,14 @@
-﻿
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace Unit
+{
   public class Unit
   {
     private UnitSettings _unitSettings;
+
     public Unit(UnitSettings unitSettings)
     {
       _unitSettings = unitSettings;
     }
   }
+}
