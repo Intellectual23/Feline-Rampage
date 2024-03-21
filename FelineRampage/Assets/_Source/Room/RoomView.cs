dd4962c7a2ff748e27976cf1ee034869a5368ecb
Egor <68235201+Intellectual23@users.noreply.github.com>
2024-03-22 01:45:06 +0300

    feat(code): add script RoomView for room prefabs on scene
    1) This script contains room info: Name and possible paths for player to go.

---
+++ FelineRampage/Assets/_Source/Room/RoomView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Room
+{
+  public class RoomView : MonoBehaviour
+  {
+    public string _name;
+    public bool _hasLeftPath;
+    public bool _hasRightPath;
+    public bool _hasFrontPath;
+    public bool _hasBackPath;
+
+    private void Start()
+    {
+      RectTransform roomRectTransform = GetComponent<RectTransform>();
+      foreach (Transform childTransform in transform) {
+        RectTransform childRectTransform = childTransform.GetComponent<RectTransform>();
+        Vector2 positionRelativeToParent = childRectTransform.anchoredPosition;
+        Debug.Log("Child position relative to parent: " + positionRelativeToParent);
+      }
+      
+    }
+  }
+}
