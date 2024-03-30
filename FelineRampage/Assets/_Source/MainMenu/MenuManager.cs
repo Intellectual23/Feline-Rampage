56be3d4afb8de66ad1a8cfed772f5328ac47ddb4
Egor <68235201+Intellectual23@users.noreply.github.com>
2024-03-30 18:52:46 +0300

    feat(code + scene): add MainMenu scene
    1) Create Main menu scene with options:
    - New Game - starts new game
    - Continue - loads save
    - Exit - exit

---
+++ FelineRampage/Assets/_Source/MainMenu/MenuManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject _settingsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _settingsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        PlayerPrefs.SetInt("Load",0);
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Continue()
+    {
+        PlayerPrefs.SetInt("Load",1);
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Settings()
+    {
+        
+    }
+    public void Exit()
+    {
+        Debug.Log("dfdkjfkdjfkdf");
+        Application.Quit();
+    }
+    
+}
